0337d5e6225132c9fe1ce98ad138db7b79bff266
MaMa-DeV2026 <mhmdtymwry473@gmail.com>
2026-07-17 18:31:53 +0330

    ل

--- lib/email.ts
+++ lib/email.ts
@@ -1,5 +1,5 @@
 import { Resend } from 'resend';
-
+console.log("RESEND_API_KEY:", process.env.RESEND_API_KEY);
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const ADMIN_EMAIL = process.env.ADMIN_EMAIL || 'mamaddev6@gmail.com';

0b9efbdb7aca5684d624880bf73b26b2fe1e1a93
MaMa-DeV2026 <mhmdtymwry473@gmail.com>
2026-07-18 23:41:36 +0330

    ذ

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+const prisma = new PrismaClient({ adapter });
 
 const IMG = (id: string) => `/images/placeholder/${id}`;
 
